e2f16b3f9dd654c4f864a65e5323a14306d68b13
Harry Lawal <harrylawal74@gmail.com>
2024-09-11 15:09:30 +0100

    update printTicket method

--- src/TicketMachine.java
+++ src/TicketMachine.java
@@ -1,5 +1,5 @@
 public class TicketMachine {
-    private int price;
+    private final int price;
     private int balance;
     private int total;
 
@@ -8,7 +8,7 @@ public class TicketMachine {
         price = ticketCost;
         // The amount of money on the ticket.
         balance = 0;
-        // The total amount of money collected by this machine.
+        // The total amount of money collected by machine.
         total = 0;
     }
 
@@ -17,16 +17,29 @@ public class TicketMachine {
         return price;
     }
 
-    //Returns the balance on the ticket
+    //Returns the balance left in the machine
     public int getBalance() {
         return balance;
     }
 
     /*
-    Receive money from a customer and add to tickets balance.
+    Receive money from a customer and add to balance.
      */
     public void insertMoney (int amount) {
         balance = balance + amount;
+    }
+
+    //prints out customers ticket
+    public void printTicket() {
+        System.out.println("Ticket name");
+        System.out.println("Ticket cost: £" + price);
+        System.out.println("Sorry no change");
+        //Update the total money collected
+        total=total+balance;
+
+        //Reset balance to zero
+        balance=0;
+
     }
 }
 
